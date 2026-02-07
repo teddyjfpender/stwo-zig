8885d7c7504d5e9be62a5d1aa1ecbf5213bec267
Theodore Pender <teddyjfpender@gmail.com>
2026-02-07 20:40:19 +0000

    Add AIR trait-like interface wrapper

--- src/core/air/mod.zig
+++ src/core/air/mod.zig
@@ -2,3 +2,16 @@ pub const accumulation = @import("accumulation.zig");
 pub const components = @import("components.zig");
 pub const Component = components.Component;
 pub const Components = components.Components;
+
+pub const AirVTable = struct {
+    components: *const fn (ctx: *const anyopaque, allocator: @import("std").mem.Allocator) anyerror![]Component,
+};
+
+pub const Air = struct {
+    ctx: *const anyopaque,
+    vtable: *const AirVTable,
+
+    pub inline fn components(self: Air, allocator: @import("std").mem.Allocator) anyerror![]Component {
+        return self.vtable.components(self.ctx, allocator);
+    }
+};
