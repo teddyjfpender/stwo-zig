a49e1e04cbe39d73655a7eeedcaf594cb35d3d62
Theodore Pender <teddyjfpender@gmail.com>
2026-02-08 13:17:20 +0000

    chore(reports): refresh benchmark and std-shims evidence

--- bench/dev/bench/data.js
+++ bench/dev/bench/data.js
@@ -2,123 +2,123 @@ window.BENCHMARK_FULL_DATA = {
   "rows": [
     {
       "family": "bit_rev",
-      "rust_prove_avg_seconds": 0.06491743066666666,
-      "rust_verify_avg_seconds": 0.0001871253333333333,
+      "rust_prove_avg_seconds": 0.066611694,
+      "rust_verify_avg_seconds": 0.00019443033333333334,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.94464,
-      "zig_over_rust_verify": 7.47271,
-      "zig_prove_avg_seconds": 0.12624100000000002,
-      "zig_verify_avg_seconds": 0.0013983333333333332
+      "zig_over_rust_prove": 2.001035,
+      "zig_over_rust_verify": 7.733704,
+      "zig_prove_avg_seconds": 0.13329233333333332,
+      "zig_verify_avg_seconds": 0.0015036666666666664
     },
     {
       "family": "eval_at_point",
-      "rust_prove_avg_seconds": 0.06890488866666666,
-      "rust_verify_avg_seconds": 0.00020043066666666667,
+      "rust_prove_avg_seconds": 0.06618215266666667,
+      "rust_verify_avg_seconds": 0.00019876366666666666,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.797957,
-      "zig_over_rust_verify": 7.171225,
-      "zig_prove_avg_seconds": 0.123888,
-      "zig_verify_avg_seconds": 0.0014373333333333332
+      "zig_over_rust_prove": 2.00204,
+      "zig_over_rust_verify": 8.108457,
+      "zig_prove_avg_seconds": 0.13249933333333333,
+      "zig_verify_avg_seconds": 0.0016116666666666666
     },
     {
       "family": "barycentric_eval_at_point",
-      "rust_prove_avg_seconds": 0.06763006966666667,
-      "rust_verify_avg_seconds": 0.00019915233333333334,
+      "rust_prove_avg_seconds": 0.06838105566666668,
+      "rust_verify_avg_seconds": 0.000198653,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.857221,
-      "zig_over_rust_verify": 6.797142,
-      "zig_prove_avg_seconds": 0.12560400000000002,
-      "zig_verify_avg_seconds": 0.0013536666666666669
+      "zig_over_rust_prove": 2.032103,
+      "zig_over_rust_verify": 5.886311,
+      "zig_prove_avg_seconds": 0.13895733333333335,
+      "zig_verify_avg_seconds": 0.0011693333333333332
     },
     {
       "family": "eval_at_point_by_folding",
-      "rust_prove_avg_seconds": 0.06677816666666665,
-      "rust_verify_avg_seconds": 0.00019565266666666668,
+      "rust_prove_avg_seconds": 0.066262125,
+      "rust_verify_avg_seconds": 0.000191889,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.821289,
-      "zig_over_rust_verify": 6.661465,
-      "zig_prove_avg_seconds": 0.12162233333333333,
-      "zig_verify_avg_seconds": 0.0013033333333333334
+      "zig_over_rust_prove": 1.806502,
+      "zig_over_rust_verify": 6.302255,
+      "zig_prove_avg_seconds": 0.11970266666666667,
+      "zig_verify_avg_seconds": 0.0012093333333333333
     },
     {
       "family": "fft",
-      "rust_prove_avg_seconds": 0.06633625,
-      "rust_verify_avg_seconds": 0.00020358366666666666,
+      "rust_prove_avg_seconds": 0.074773486,
+      "rust_verify_avg_seconds": 0.00019369466666666667,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.793901,
-      "zig_over_rust_verify": 6.421602,
-      "zig_prove_avg_seconds": 0.11900066666666666,
-      "zig_verify_avg_seconds": 0.0013073333333333333
+      "zig_over_rust_prove": 1.658837,
+      "zig_over_rust_verify": 7.045453,
+      "zig_prove_avg_seconds": 0.124037,
+      "zig_verify_avg_seconds": 0.0013646666666666668
     },
     {
       "family": "field",
-      "rust_prove_avg_seconds": 0.067309778,
-      "rust_verify_avg_seconds": 0.000204222,
+      "rust_prove_avg_seconds": 0.06711348633333333,
+      "rust_verify_avg_seconds": 0.00020061166666666668,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.775819,
-      "zig_over_rust_verify": 6.499463,
-      "zig_prove_avg_seconds": 0.11952999999999998,
-      "zig_verify_avg_seconds": 0.0013273333333333334
+      "zig_over_rust_prove": 1.668646,
+      "zig_over_rust_verify": 5.885334,
+      "zig_prove_avg_seconds": 0.11198866666666667,
+      "zig_verify_avg_seconds": 0.0011806666666666667
     },
     {
       "family": "fri",
-      "rust_prove_avg_seconds": 0.067601556,
-      "rust_verify_avg_seconds": 0.00020479166666666668,
+      "rust_prove_avg_seconds": 0.06704936166666667,
+      "rust_verify_avg_seconds": 0.00019219433333333332,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.795235,
-      "zig_over_rust_verify": 6.396745,
-      "zig_prove_avg_seconds": 0.12136066666666667,
-      "zig_verify_avg_seconds": 0.0013100000000000002
+      "zig_over_rust_prove": 1.644634,
+      "zig_over_rust_verify": 7.459464,
+      "zig_prove_avg_seconds": 0.11027166666666666,
+      "zig_verify_avg_seconds": 0.0014336666666666664
     },
     {
       "family": "lookups",
-      "rust_prove_avg_seconds": 0.06724248633333334,
-      "rust_verify_avg_seconds": 0.000208306,
+      "rust_prove_avg_seconds": 0.06632898633333334,
+      "rust_verify_avg_seconds": 0.00018763866666666668,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.762566,
-      "zig_over_rust_verify": 6.232818,
-      "zig_prove_avg_seconds": 0.11851933333333332,
-      "zig_verify_avg_seconds": 0.0012983333333333332
+      "zig_over_rust_prove": 1.883671,
+      "zig_over_rust_verify": 7.09875,
+      "zig_prove_avg_seconds": 0.124942,
+      "zig_verify_avg_seconds": 0.0013319999999999999
     },
     {
       "family": "merkle",
-      "rust_prove_avg_seconds": 0.06849116666666667,
-      "rust_verify_avg_seconds": 0.000174014,
+      "rust_prove_avg_seconds": 0.06661173599999999,
+      "rust_verify_avg_seconds": 0.00019237533333333332,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 3.13749,
-      "zig_over_rust_verify": 7.208232,
-      "zig_prove_avg_seconds": 0.21489033333333332,
-      "zig_verify_avg_seconds": 0.0012543333333333334
+      "zig_over_rust_prove": 1.774677,
+      "zig_over_rust_verify": 6.206617,
+      "zig_prove_avg_seconds": 0.11821433333333332,
+      "zig_verify_avg_seconds": 0.001194
     },
     {
       "family": "prefix_sum",
-      "rust_prove_avg_seconds": 0.068299361,
-      "rust_verify_avg_seconds": 0.00020477733333333333,
+      "rust_prove_avg_seconds": 0.06531448599999999,
+      "rust_verify_avg_seconds": 0.00020144433333333333,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 1.982444,
-      "zig_over_rust_verify": 8.459595,
-      "zig_prove_avg_seconds": 0.13539966666666667,
-      "zig_verify_avg_seconds": 0.0017323333333333333
+      "zig_over_rust_prove": 1.91468,
+      "zig_over_rust_verify": 6.039717,
+      "zig_prove_avg_seconds": 0.12505633333333332,
+      "zig_verify_avg_seconds": 0.001216666666666667
     },
     {
       "family": "pcs",
-      "rust_prove_avg_seconds": 0.068803208,
-      "rust_verify_avg_seconds": 0.00018645833333333332,
+      "rust_prove_avg_seconds": 0.06524015266666668,
+      "rust_verify_avg_seconds": 0.000196097,
       "zig_over_rust_proof_wire_bytes": 1.0,
-      "zig_over_rust_prove": 2.043519,
-      "zig_over_rust_verify": 9.442682,
-      "zig_prove_avg_seconds": 0.14060066666666668,
-      "zig_verify_avg_seconds": 0.0017606666666666667
+      "zig_over_rust_prove": 1.784423,
+      "zig_over_rust_verify": 6.423692,
+      "zig_prove_avg_seconds": 0.116416,
+      "zig_verify_avg_seconds": 0.0012596666666666667
     }
   ],
   "schema_version": 1,
   "source_report": "vectors/reports/benchmark_full_report.json",
   "summary": {
-    "avg_zig_over_rust_prove": 1.973826,
-    "avg_zig_over_rust_verify": 7.160334,
+    "avg_zig_over_rust_prove": 1.83375,
+    "avg_zig_over_rust_verify": 6.744523,
     "failure_count": 0,
     "families": 11,
-    "max_zig_over_rust_prove": 3.13749,
-    "max_zig_over_rust_verify": 9.442682
+    "max_zig_over_rust_prove": 2.032103,
+    "max_zig_over_rust_verify": 8.108457
   }
 };
